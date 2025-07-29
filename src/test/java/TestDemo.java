f0248c6697c1ede3d612c78b25ed5e5919a9fb42
Pavel Khamenia <khameniapavel@gmail.com>
2025-07-29 20:40:52 +0300

    Add HerokuApp

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,14 +10,18 @@ public class TestDemo {
   @Test
   public void seleniumTest() {
     ChromeOptions options = new ChromeOptions();
+    HashMap<String, Object> chromePrefs = new HashMap<>();
+    chromePrefs.put("credentials_enable_service", false);
+    chromePrefs.put("profile.password_manager_enabled", false);
+    options.setExperimentalOption("prefs", chromePrefs);
+    options.addArguments("--disable-popup-blocking");
+    options.addArguments("--disable-infobars");
     options.addArguments("--start-maximized");//максимальный экран
     options.addArguments("--incognito");//инкогнито
     options.addArguments("--disable-notification");//убрать уведомленияя
     options.addArguments("--headless");//без открытия интерфейса
-    options.addArguments("--headless");//без открытия интерфейса
+    WebDriver driver = new ChromeDriver(options);
 
-
-    WebDriver driver = new ChromeDriver();
     driver.get("https://the-enternet.herokuapp.com/");
 
     driver.quit();
