5966bf21af2f813593f8ae3b0697043ff40e50e8
Pavel Khamenia <khameniapavel@gmail.com>
2025-07-29 20:26:30 +0300

    Add HerokuApp

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -9,7 +9,13 @@ public class TestDemo {
   @Test
   public void seleniumTest() {
     ChromeOptions options = new ChromeOptions();
-    options.addArguments("");
+    options.addArguments("--start-maximized");//максимальный экран
+    options.addArguments("--incognito");//инкогнито
+    options.addArguments("--disable-notification");//убрать уведомленияя
+    options.addArguments("--headless");//без открытия интерфейса
+    options.addArguments("--headless");//без открытия интерфейса
+
+
     WebDriver driver = new ChromeDriver();
     driver.get("https://the-enternet.herokuapp.com/");
 
