80be06eae8d624d5dec66e58dd2371186d304276
Pavel Khamenia <khameniapavel@gmail.com>
2025-07-29 20:20:32 +0300

    Add HerokuApp

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class TestDemo {
@@ -7,6 +8,8 @@ public class TestDemo {
 
   @Test
   public void seleniumTest() {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("");
     WebDriver driver = new ChromeDriver();
     driver.get("https://the-enternet.herokuapp.com/");
 
